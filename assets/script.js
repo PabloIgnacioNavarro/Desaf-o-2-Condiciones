cbe47cc6cbcfe7fc0061359c79c889562f7b7421
Pablo Navarro <ignacionavarropablo@gmail.com>
2024-03-11 21:23:55 -0300

    primer commit

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+edad = Number(prompt("Ingresa tu edad"));
+console.log(edad);
+if (edad < 18) {
+  console.log("Lo siento, No puedes entrar a esta pagina.");
+}
---
+++ assets/script2.js
@@ -0,0 +1,9 @@
+var parrafo = document.querySelector("p");
+var contraseña = document.querySelector("primero").value;
+if (contraseña == 1) {
+  alert("adelante");
+} else if (contraseña == 2) {
+  alert("pase");
+} else {
+  alert("fuera");
+}
